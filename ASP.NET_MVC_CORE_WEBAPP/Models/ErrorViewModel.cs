a72a073b192cfa20343ad0c31cf078c08fd09024
LocDepTry <letanloc.12082004@gmail.com>
2023-12-28 03:16:27 +0700

    khởi tạo

---
+++ ASP.NET_MVC_CORE_WEBAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_MVC_CORE_WEBAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
